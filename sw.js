776a4f6fbb5ae1cc09d47da4628f0b9146cc0d11
1701-cmd <gabriel.leal9813@gmail.com>
2026-02-21 11:56:44 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,39 @@
-const CACHE_NAME = 'fainas-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
-  'https://cdn-icons-png.flaticon.com/512/1584/1584892.png'
+const CACHE_NAME = "sistema-fainas-v1";
+
+const urlsToCache = [
+  "/Sistema-fainas/",
+  "/Sistema-fainas/index.html",
+  "/Sistema-fainas/style.css",
+  "/Sistema-fainas/script.js",
+  "/Sistema-fainas/manifest.json",
+  "/Sistema-fainas/icon-192.png",
+  "/Sistema-fainas/icon-512.png"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
